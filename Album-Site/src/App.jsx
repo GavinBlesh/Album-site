6497d35e2e583efa6b7665aa9e1f5cff847fb95e
GavinBlesh <bleshgavin@gmail.com>
2025-09-29 22:35:40 -0500

    Personal rec header; quickfix on some formatting

--- Album-Site/src/App.jsx
+++ Album-Site/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Heading from './Heading'
 import Button from './Button'
 import Line from './Line'
+import RecHeading from './RecHeading'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -14,6 +15,7 @@ function App() {
       <Heading />
       <Button />
       <Line />
+      <RecHeading />
     </div>
   )
 }
---
+++ Album-Site/src/RecHeading.jsx
@@ -0,0 +1,10 @@
+export default
+
+function Heading(){
+    return (
+        <h2 className='h2'>
+            Creator's Pick
+
+        </h2>
+    );
+}
